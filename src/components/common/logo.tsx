af5e9824ee0d4a8c4babfcce2bf1648b0a5b4e48
Jesse Ogbonna <jessesamuel84@gmail.com>
2024-10-14 09:12:31 +0100

    initial

--- src/components/common/logo.tsx
+++ src/components/common/logo.tsx
@@ -11,25 +11,13 @@ export default function Logo({
   return (
     <div>
       <div className='flex gap-x-2 lg:gap-x-3'>
-        <div className='relative w-[36px] h-[36px] lg:w-[48px] lg:h-[48px] flex-shrink-0'>
+        <div className='relative w-[128px] h-[56px] flex-shrink-0'>
           <Image
             src={'/logo.svg'}
             alt='logo'
             fill
           />
         </div>
-
-        <p
-          className={cn(
-            'font-plus-jakarta-sans font-bold text-gradient text-sm lg:text-[30px] pt-[3px] lg:pt-2',
-            {
-              'text-white': white,
-              '!text-lg !pt-1': small,
-            }
-          )}
-        >
-          instantotp
-        </p>
       </div>
     </div>
   );
